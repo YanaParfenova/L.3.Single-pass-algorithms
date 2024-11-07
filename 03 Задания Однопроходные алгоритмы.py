6aaf5ec28e6acaaed03946a381911a5dbf13620a
YanaParfenova <yyaannaa0708@gmail.com>
2024-11-08 02:24:23 +0400

    first commit

---
+++ 03 Задания Однопроходные алгоритмы.py
@@ -0,0 +1,64 @@
+"""
+Написать программу, которая читая символы из бесконечной последовательности
+(эмулируется конечным файлом), распознает, преобразует и выводит на экран
+объекты по определенному правилу. Объекты разделены пробелами. Преобразование
+делать по возможности через словарь. Для упрощения под выводом числа прописью
+подразумевается последовательный вывод всех цифр числа. Регулярные выражения
+использовать нельзя.
+Вариант 17.
+Последовательности натуральных чисел, расположенных в порядке не возрастания.
+Для каждой такой последовательности минимальное число вывести прописью.
+"""
+
+# Cловарь, отображающий цифры (0-9) на их словесные представления
+digits_map = {
+    0: "ноль", 1: "один", 2: "два", 3: "три", 4: "четыре",
+    5: "пять", 6: "шесть", 7: "семь", 8: "восемь", 9: "девять"
+}
+
+
+def process_sequence(filename):
+    # Обработка файла
+    try:
+        with open(filename, 'r') as file:
+            sequence_str = file.read().strip()
+    except FileNotFoundError:
+        print(f"Ошибка: файл '{filename}' не найден.")
+        return
+    
+    # Разбиение строки на числа  
+    numbers = [int(x) for x in sequence_str.split()]
+
+    if not numbers:
+        print("Последовательность пуста.")
+        return
+
+    # Инициализация текущей невозрастающей подпоследовательности первым числом
+    current_sequence = [numbers[0]]
+    # Инициализация минимального числа текущей подпоследовательности
+    min_num = numbers[0]
+    
+    # Цикл по последовательности
+    for i in range(1, len(numbers)):
+        if numbers[i] <= current_sequence[-1]:
+            current_sequence.append(numbers[i])
+            min_num = min(min_num, numbers[i])
+        else:
+            output_min(min_num)
+            current_sequence = [numbers[i]]
+            min_num = numbers[i]
+
+    output_min(min_num)
+
+
+# Вывод числа прописью по цифрам
+def output_min(number):
+    num_str = str(number)
+    result = ""
+    for digit in num_str:
+        result += digits_map[int(digit)] + " "
+    print(result.strip())
+
+
+filename = "input.txt"
+process_sequence(filename)
